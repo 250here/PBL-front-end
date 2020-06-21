7811a579ff9c7af63945b445b1e4d828a2ccff3f
FDYZHao <17302010032@fudan.edu.cn>
2020-06-21 12:10:29 +0800

    00

--- src/app/service/discuss.service.ts
+++ src/app/service/discuss.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Constants} from '../common/Constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DiscussService {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient,
+    private constants: Constants,
+  ) { }
+  // newDiscuss(){
+  //   let httpoptions = this.constants.jsonTokenOptions();
+  //   return this.httpClient.post(this.constants.urls.ADD_PROJECT_URL, project, httpoptions);
+  // }
 }
